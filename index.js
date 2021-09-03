079746ba05566dca9b6b9addf73b192fcf6ee35f
Daniel Vilardell <dani@happyscribe.co>
2021-09-03 16:44:46 +0200

    refactor: set up project ready for heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const tools = require('./scripts/random_playlist')
 const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false})
 
